760b0d2e41edf18bcee0d131a6b761e5d9bdcc01
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-06-29 15:19:52 -0700

    save

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,30 +1,36 @@
-var textbox = document.querySelectorAll("search");
+var cityName = document.getElementById("search");
+var btn = document.getElementById("pressSearch");
+var weather = document.querySelector("ul");
 
-// search.addEventListener("click");
-
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=Tucson&units=imperial&appid=7e663f4b1d9924a34d2b5f917f979be1")
-.then(function(weather){
-return weather.json()
-}).then(function(data){
-console.log(data)
-console.log(data.city.coord.lat)
-
-
-fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${data.city.coord.lat}&lon=${data.city.coord.lon}&appid=7e663f4b1d9924a34d2b5f917f979be1`)
-.then(function(anything){
-return anything.json()
+btn.addEventListener("click", function (event) {
+  event.preventDefault();
+  var weatherUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName.value +
+    "&appid=b40af81dd3d94b19f0949cf426fd789a";
+  console.log(event.target);
+  fetch(weatherUrl)
+    .then(function (taco) {
+      return taco.json();
+    })
+    .then(function (data) {
+      var list = document.createElement("li");
+      console.log(data.main);
+      var header = document.createElement("h3");
+    });
+  var weatherUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName.value +
+    "&appid=b40af81dd3d94b19f0949cf426fd789a";
+  console.log(event.target);
+  fetch(weatherUrl)
+    .then(function (weather) {
+      return weather.json();
+    })
+    .then(function (data) {
+      for (let i = 0; i < data.list.length; i++) {
+        const element = data.list[i];
+        console.log(element);
+    }
+});
 })
-// .then(function(UV){
-  //console.log(UV)
-
-
-
-
-})
-
-
-
-// https://api.openweathermap.org/data/2.5/forecast?q=Tucson&appid=7e663f4b1d9924a34d2b5f917f979be1
-//  https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=7e663f4b1d9924a34d2b5f917f979be1
-//  https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-//  https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
